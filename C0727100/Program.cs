be02a0d2bb864cec743fb47fd9bbcfca786ea872
visualstudio <visualstudio@122-PC09>
2019-03-08 18:57:32 -0500

    6.57 pm

---
+++ C0727100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0727100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
